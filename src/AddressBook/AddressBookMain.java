076949d32ee3d4e68b13027e3ff96bcc13d6cfae
nnalin04 <nnalin04@gmail.com>
2020-08-10 21:51:56 +0530

    added: the print statement

--- src/AddressBook/AddressBookMain.java
+++ src/AddressBook/AddressBookMain.java
@@ -3,6 +3,6 @@ package AddressBook;
 public class AddressBookMain {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Welcome to AddressBook");
     }
 }
